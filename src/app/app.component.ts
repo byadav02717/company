bd15941c8a5d0d61c6d6f61c79238d862015268b
Bivash Yadav <byadav02717@gmail.com>
2022-10-14 19:47:24 -0500

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+
+  constructor(){
+
+  }
+
+  menuVariable:boolean=false;
+  openMenu(){
+    this.menuVariable = !this.menuVariable;
+  }
+
+}
